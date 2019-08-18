0caab2d49d4937ebec56dfb8a88abab2e20928e5
weigenie <ieeweigen@hotmail.com>
2019-08-19 01:33:10 +0800

    Level-2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
 
     static Scanner sc = new Scanner(System.in);
+    static List<String> lst = new ArrayList<>();
+    static String tab = "\t____________________________________________________________";
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -11,21 +15,34 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
-        String tab = "\t____________________________________________________________";
 
-        System.out.println(tab + "\t\n\tHello! I'm Duke\n\tWhat can I do for you?\n" + tab);
-        while (true) {
+        print("Hello! I'm Duke\n\tWhat can I do for you?");
+        boolean quit = false;
+        while (!quit) {
             String input = sc.nextLine();
-            if (input.equals("bye")) {
-                System.out.println(tab);
-                System.out.println("\tBye. Hope to see you again soon!");
-                System.out.println(tab);
-                break;
-            } else {
-                System.out.println(tab);
-                System.out.println("\t" + input);
-                System.out.println(tab);
+            switch(input) {
+                case "bye":
+                    print("Bye. Hope to see you again soon!");
+                    quit = true;
+                    break;
+                case "list":
+                    System.out.println(tab);
+                    for (int i = 0; i < lst.size(); i++) {
+                        System.out.println("\t "  + (i + 1) + ". " + lst.get(i));
+                    }
+                    System.out.println(tab + "\n");
+                    break;
+                default:
+                    print(" added: " + input);
+                    lst.add(input);
+                    break;
             }
         }
+    }
+
+    static void print(String txt) {
+        System.out.println(tab);
+        System.out.println("\t" + txt);
+        System.out.println(tab + "\n");
     }
 }
