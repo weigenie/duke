90ca47dad72df6867793f1736ea5db31b555b81b
weigenie <ieeweigen@hotmail.com>
2019-08-19 03:15:20 +0800

    Level 5. Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,47 +22,63 @@ public class Duke {
             String input = sc.next();
             String rest;
             String[] split;
-            switch(input) {
-                case "bye":
-                    print("Bye. Hope to see you again soon!");
-                    quit = true;
-                    break;
-                case "list":
-                    System.out.println(tab);
-                    System.out.println(" Here are the tasks in your list");
-                    for (int i = 0; i < lst.size(); i++) {
-                        System.out.println("\t "  + (i + 1) + ". " + lst.get(i));
-                    }
-                    System.out.println(tab + "\n");
-                    break;
-                case "done":
-                    int doneNum = Integer.parseInt(sc.next()) - 1;
-                    lst.get(doneNum).markAsDone();
-                    print(" Nice! I've marked this task as done:\n\t   " + lst.get(doneNum));
-                    break;
-                case "todo":
-                    String desc = sc.nextLine();
-                    lst.add(new Todo(desc));
-                    print(" Got it. I've added this task:\n\t   " + lst.get(lst.size() - 1) + "\n\tNow you have " +
-                            lst.size() + " tasks in the list.");
-                    break;
-                case "deadline":
-                    rest = sc.nextLine();
-                    split = rest.split("/by ");
-                    lst.add(new Deadline(split[0], split[1]));
-                    print(" Got it. I've added this task:\n\t   " + lst.get(lst.size() - 1) + "\n\tNow you have " +
-                            lst.size() + " tasks in the list.");
-                    break;
-                case "event":
-                    rest = sc.nextLine();
-                    split = rest.split("/at ");
-                    lst.add(new Event(split[0], split[1]));
-                    print(" Got it. I've added this task:\n\t   " + lst.get(lst.size() - 1) + "\n\tNow you have " +
-                            lst.size() + " tasks in the list.");
-                    break;
-                default:
-                    System.out.println("error");
-                    break;
+            try {
+                switch (input) {
+                    case "bye":
+                        print("Bye. Hope to see you again soon!");
+                        quit = true;
+                        break;
+                    case "list":
+                        System.out.println(tab);
+                        System.out.println(" Here are the tasks in your list");
+                        for (int i = 0; i < lst.size(); i++) {
+                            System.out.println("\t " + (i + 1) + ". " + lst.get(i));
+                        }
+                        System.out.println(tab + "\n");
+                        break;
+                    case "done":
+                        int doneNum = Integer.parseInt(sc.next()) - 1;
+                        lst.get(doneNum).markAsDone();
+                        print(" Nice! I've marked this task as done:\n\t   " + lst.get(doneNum));
+                        break;
+                    case "todo":
+                        String desc = sc.nextLine();
+                        if (desc.isEmpty()) {
+                            throw new DukeException(" ☹ OOPS!!! The description of a todo cannot be empty.");
+                        }
+                        lst.add(new Todo(desc));
+                        print(" Got it. I've added this task:\n\t   " + lst.get(lst.size() - 1) + "\n\tNow you have " +
+                                lst.size() + " tasks in the list.");
+                        break;
+                    case "deadline":
+                        rest = sc.nextLine();
+                        if (rest.isEmpty()) {
+                            throw new DukeException(" ☹ OOPS!!! The description of a deadline cannot be empty.");
+                        } else if (!rest.contains("/by")) {
+                            throw new DukeException(" ☹ OOPS!!! Deadline input should include '/by'.");
+                        }
+                        split = rest.split("/by ");
+                        lst.add(new Deadline(split[0], split[1]));
+                        print(" Got it. I've added this task:\n\t   " + lst.get(lst.size() - 1) + "\n\tNow you have " +
+                                lst.size() + " tasks in the list.");
+                        break;
+                    case "event":
+                        rest = sc.nextLine();
+                        if (rest.isEmpty()) {
+                            throw new DukeException(" ☹ OOPS!!! The description of an event cannot be empty.");
+                        } else if (!rest.contains("/at")) {
+                            throw new DukeException(" ☹ OOPS!!! Deadline input should include '/at'.");
+                        }
+                        split = rest.split("/at ");
+                        lst.add(new Event(split[0], split[1]));
+                        print(" Got it. I've added this task:\n\t   " + lst.get(lst.size() - 1) + "\n\tNow you have " +
+                                lst.size() + " tasks in the list.");
+                        break;
+                    default:
+                        throw new DukeException(" ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException e) {
+                print (e.getMessage());
             }
         }
     }
